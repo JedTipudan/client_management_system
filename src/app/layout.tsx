1f2ded597b3495858a21b7e6bfe3aec93dc45945
Jed Tipudan <jed.tipudan@dorsu.edu.ph>
2026-02-20 15:13:48 +0800

    Added sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Sidebar from "./components/Sidebar"; // Your sidebar component
+import Sidebar from "../components/Sidebar"; // Your sidebar component
 
 const inter = Inter({ subsets: ["latin"] });
 
